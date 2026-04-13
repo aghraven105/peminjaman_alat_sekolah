6946ec46f06665098fc4408018992946d3fc40e5
aghraven105 <aghistsmkmerdeka23@gmail.com>
2026-04-13 15:35:04 +0700

    Create migration file for categories table

---
+++ database/migrations/2026_04_13_083418_create_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
